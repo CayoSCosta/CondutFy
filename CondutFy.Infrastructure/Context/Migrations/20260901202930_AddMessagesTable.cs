c28e542197502b412776fd510cf0b07d973abcb0
ACT Cayo Santos da Costa <act.ccosta@ourinvest.com.br>
2026-09-01 17:30:56 -0300

    Atualizações

---
+++ CondutFy.Infrastructure/Context/Migrations/20260901202930_AddMessagesTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CondutFy.Infrastructure.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMessagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TenantId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ChannelIntegrationId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    SenderPhone = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    ExternalMessageId = table.Column<string>(type: "TEXT", nullable: false),
+                    IsFromBot = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
